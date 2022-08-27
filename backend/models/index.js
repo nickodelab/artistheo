accaea1b160b1d585953ba68dabd5794af0011a6
Nicolás Martín Acosta <nico@somoslaclave.com>
2022-08-27 15:31:06 +0200

    only frontend to deploy on netlify

--- backend/models/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const { Artist } = require('./schemas')
-
-module.exports = {
-    Artist: mongoose.model('Artist', Artist)
-}
--- backend/routes/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    // CRUD Users
-    getArtistsByType: require('./get-artists'),
-}
